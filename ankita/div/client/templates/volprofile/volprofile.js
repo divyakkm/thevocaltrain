74d6c2244ab454af01bf74bc818ebd565e0b1683
AnkitaBhosle <ankita.bhosle@ischool.berkeley.edu>
2015-04-05 09:39:19 -0700

    latest volprofile

--- ankita/div/client/templates/volprofile/volprofile.js
+++ ankita/div/client/templates/volprofile/volprofile.js
@@ -49,37 +49,79 @@ Template.showModal.helpers({
 
 	act: function () {
 		console.log("inside act");
-//		var ref2 = Activity.find({
-			//			_id: LessonBlock.find({
-			//				_id: SessionList.find({
-			//					id: "551cae3cb1ddc9927db19e89"
-			//				}).fetch()[0].lesson_id
-			//			}).fetch()[0].activity_id
-			//}).fetch()[0];
-		var ref2 = Activity.find({
-			_id: "L1.0.1"
-		}).fetch()[0];
+		var ref2 = Activity.find({lesson_id: SessionList.find({_id: "551cae3cb1ddc9927db19e89"}).fetch()[0].lesson_id});
 		console.log(ref2);
 		return ref2;
+	},
+
+	cmt: function () {
+		console.log("WAITWUT");
+		
+		// var content = $('#currentnotes').value;
+		// console.log(content);
+		// return content;
 	}
 });
 
 Template.showModal.events({
-	'click .save': function (temp, tmpl) {
-		console.log("inside showmodal event");
-		console.log(temp);
-	}
+	// 'click .save': function (temp, tmpl) {
+	// 	console.log("inside showmodal event");
+	// 	console.log(temp);
+	// },
+
+	'click .save': function () {
+		console.log("step1");
+		updateSessionList();
+		//SessionList.update({_id: "551cae3cb1ddc9927db19e89"}, {$set: {notes: "5"}});
+		//console.log("step 2");
+
+	} 
 })
 
-//Template.showModal.evt = function () {
-//	// run a query to the database
-//	// console.log(modalEvent);
-//	var modalEvent = SessionList.findOne({
-//		_id: Session.get('volunteer_id')
-//	});
-//	console.log(modalEvent);
-//	return modalEvent;
-//}
+
+
+// Template.editEvent.events({
+// 	'click .save': function (evt, tmpl) {
+// 		console.log(evt);
+// 		//                updateCalEvent(Session.get('editing_calevent'), tmpl.find('.title').value);
+// 		updateCalEvent(Session.get('editing_calevent'), 'true');
+// 		Session.set('editing_calevent', null);
+// 		Session.set('showEditEvent', false);
+// 	},
+// 	'click .close': function (evt, tmpl) {
+// 		Session.set('editing_calevent', null);
+// 		Session.set('showEditEvent', false);
+// 		$('#EditEventModal').modal("hide");
+// 	},
+// 	'click .remove': function (evt, tmpl) {
+// 		removeCalEvent(Session.get('editing_calevent'));
+// 		Session.set('editing_calevent', null);
+// 		Session.set('showEditEvent', false);
+// 		$('#EditEventModal').modal("hide");
+// 	}
+// })
+
+var updateSessionList = function () {
+	SessionList.update({_id: "551cae3cb1ddc9927db19e89"}, {$set: {notes: "17"}});
+	console.log("step2");
+}
+
+// var updateCalEvent = function (id, title) {
+// 	CalEvents.update(id, {
+// 		$set: {
+// 			assigned: 'true'
+// 		}
+// 	});
+// 	console.log(id + " " + Session.get('volunteer_id'));
+// 	StudentVolunteer.insert({
+// 		student_id: id,
+// 		voluteer_id: Session.get('volunteer_id'),
+// 		//start:CalEvents.findOne({_id : id},{start:1,_id:0})
+// 		start: 10
+// 	});
+// 	updateCalendar();
+// }
+
 
 Template.sessiontable.events({
 	'click tbody > tr': function (event, view) {
