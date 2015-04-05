8981e559a0f833d70d6f4ac50e3103dc39e07e1d
divyakkm <divyakkm@gmail.com>
2015-04-04 18:41:55 -0700

    Activity helper fixed.

--- ankita/div/both/controllers/volprofile.js
+++ ankita/div/both/controllers/volprofile.js
@@ -50,18 +50,19 @@ TabularTables.Books = new Tabular.Table({
 });
 
 VolprofileController = AppController.extend({
-		waitOn: function () {
+	waitOn: function () {
 		return this.subscribe('calevents');
 	},
 	onBeforeAction: function (pause) {
 		AccountsTemplates.ensureSignedIn.call(this, pause);
 	},
-	data:  {
-			SessionList: SessionList.find({}),
-			CalEvents: CalEvents.find({}),
-			StudentVolunteer: StudentVolunteer.find({}),
-			LessonBlock: LessonBlock.find({}),
-			Assessment: Assessment.find({})
+	data: {
+		SessionList: SessionList.find({}),
+		CalEvents: CalEvents.find({}),
+		StudentVolunteer: StudentVolunteer.find({}),
+		LessonBlock: LessonBlock.find({}),
+		Activity: Activity.find({}),
+		Assessment: Assessment.find({})
 	},
 	onAfterAction: function () {
 		Meta.setTitle('Volunteer Profile');
--- ankita/div/server/permissions/items.js
+++ ankita/div/server/permissions/items.js
@@ -57,3 +57,15 @@ LessonBlock.allow({
 		return userId;
 	}
 });
+
+Activity.allow({
+	'insert': function (userId, doc) {
+		return userId;
+	},
+	'update': function (userId, doc, fields, modifier) {
+		return userId;
+	},
+	'remove': function (userId, doc) {
+		return userId;
+	}
+});
