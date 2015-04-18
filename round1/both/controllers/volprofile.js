0e88d1a2758b6070f0b575b47ced148c881167e9
divyakkm <divyakkm@gmail.com>
2015-04-17 22:12:43 -0700

    Added backup code

---
+++ round1/both/controllers/volprofile.js
@@ -0,0 +1,88 @@
+// console.log("in both folder");
+TabularTables = {};
+Meteor.isClient && Template.registerHelper('TabularTables', TabularTables);
+
+TabularTables.Sessions = new Tabular.Table({
+	name: "Session List",
+	collection: SessionList,
+	columns: [
+		{
+			data: "date",
+			title: "Date"
+		},
+		{
+			data: "time",
+			title: "Time"
+		},
+		{
+			data: "lesson_name",
+			title: "Lesson"
+		},
+		{
+			data: "assigned_student",
+			title: "Student"
+		},
+		{
+			data: "assigned_student_id",
+			title: "Student",
+			visible: false
+		},
+		{
+			data: "lesson_id",
+			title: "Lesson Id",
+			visible: false
+		},
+		{
+			data: "volunteer_id",
+			title: "Volunteer Id",
+			visible: false
+		},
+		{
+			data: "notes",
+			title: "Notes",
+			visible: false
+		},
+		{
+			data: "this._id",
+			visible: false
+		},
+		{
+			data: "submittedAt",
+			title: "Submitted At",
+			visible: false
+		}
+
+// 	{
+//   tmpl: Meteor.isClient && Template.sessiontable
+// }
+	]
+});
+
+VolprofileController = AppController.extend({
+	waitOn: function () {
+		return [
+		this.subscribe('StudentVolunteer'),
+		this.subscribe('calevents')]
+	},
+
+	onBeforeAction: function (pause) {
+		AccountsTemplates.ensureSignedIn.call(this, pause);
+	},
+	data: {
+		SessionList: SessionList.find({}),
+		CalEvents: CalEvents.find({}),
+		StudentVolunteer: StudentVolunteer.find({}),
+		LessonBlock: LessonBlock.find({}),
+		Assessment: Assessment.find({}),
+		Activity: Activity.find({})
+	},
+	onAfterAction: function () {
+		Meta.setTitle('Volunteer Profile');
+	}
+});
+
+VolprofileController.events({
+	'click [data-action=doSomething]': function (event, template) {
+		event.preventDefault();
+	}
+});
---
+++ round1/client/templates/volprofileReg/volprofileReg.js
@@ -0,0 +1,17 @@
+Template.volprofileReg.rendered = function () {
+
+};
+
+AppController = RouteController.extend({
+	layoutTemplate: 'appLayout'
+});
+
+AppController.events({
+	'click [data-action=logout]': function () {
+		AccountsTemplates.logout();
+	}
+});
+
+ResourcesController = AppController.extend({
+	layoutTemplate: 'appLayout'
+});
